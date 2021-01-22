785fb0594af449438414f2052876a22f5866523d
Ahmed Fouad <ahmed77fouad23@gmail.com>
2021-01-22 15:16:49 +0200

    Download All cdn links & Add Ui Search

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ const port = 8080 ;
 const app = express();
 const path = require("path");
 const db = mongoose.connection;
-let db_url = 'mongodb+srv://ahmed_fouad:12345Aa@cluster0.tpy6v.mongodb.net/<dbname>?retryWrites=true&w=majority'
+let db_url = 'mongodb://localhost:27017/Accounting';
+// let db_url = 'mongodb+srv://ahmed_fouad:12345Aa@cluster0.tpy6v.mongodb.net/<dbname>?retryWrites=true&w=majority'
 
 const flash = require("connect-flash");
 
